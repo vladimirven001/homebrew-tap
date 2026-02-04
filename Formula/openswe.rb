09223c14bdd138d6bd84591d32f48aadf65bdcc4
vlad <vladimirven001@gmail.com>
2026-02-03 19:51:47 -0500

    remove mac x64

--- Formula/openswe.rb
+++ Formula/openswe.rb
@@ -1,24 +1,17 @@
 # Homebrew formula for openswe
 # This file should be placed in your homebrew-tap repo at:
 # https://github.com/vladimirven001/homebrew-tap/Formula/openswe.rb
-
 class Openswe < Formula
   desc "AI coding agent orchestration tool for tackling GitHub issues with automatic worktree creation"
   homepage "https://github.com/vladimirven001/openswe"
   version "1.0.0"
   license "ISC"
-
   on_macos do
     on_arm do
       url "https://github.com/vladimirven001/openswe/releases/download/v#{version}/openswe-darwin-arm64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
     end
-    on_intel do
-      url "https://github.com/vladimirven001/openswe/releases/download/v#{version}/openswe-darwin-x64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_X64"
-    end
   end
-
   on_linux do
     on_arm do
       url "https://github.com/vladimirven001/openswe/releases/download/v#{version}/openswe-linux-arm64.tar.gz"
@@ -29,11 +22,9 @@ class Openswe < Formula
       sha256 "PLACEHOLDER_SHA256_LINUX_X64"
     end
   end
-
   def install
     bin.install "openswe"
   end
-
   test do
     assert_match "openswe", shell_output("#{bin}/openswe --version", 2)
   end
